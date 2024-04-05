4a81e54c55fbf5fe4ea76f65a4358f9ef6836c18
Pedro Mendoza Full Stack DEV <157172448+jppe1994@users.noreply.github.com>
2024-04-05 23:36:09 +0000

    to do list terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Tareas from "./tareas";
 
 //create your first component
 const Home = () => {
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div id="TodoList">
+			<h1>
+				To do list
+			</h1>
+			<Tareas id='Tareas'/>
+			
 		</div>
+		<div id='linea1'>
+	
+		
+	  </div>
+	  <div id='linea2'>
+	  
+	  </div>
+		</>
+
+		
 	);
 };
 
